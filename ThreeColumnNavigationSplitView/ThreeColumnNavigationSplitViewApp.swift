e5fff7c21274ec36f4bc22d57009dd1c8256eda5
Ramill <ramill_ibragimov@icloud.com>
2022-10-02 09:22:51 +0300

    Initial Commit

---
+++ ThreeColumnNavigationSplitView/ThreeColumnNavigationSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeColumnNavigationSplitViewApp.swift
+//  ThreeColumnNavigationSplitView
+//
+//  Created by Ramill Ibragimov on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ThreeColumnNavigationSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
